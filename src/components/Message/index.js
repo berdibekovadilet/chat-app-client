8ef0ff5041a4211308d1b9d2adda63af46da7948
adilet berdibekov <berdibekovadilet@gmail.com>
2022-12-28 10:42:55 +0600

    updated Message component

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,20 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { formatDistance } from "date-fns";
+import ruLocale from "date-fns/locale/ru";
+import classNames from "classnames";
 
 import "./Message.scss";
 
-const Message = ({ avatar, user, text, date }) => {
+const Message = ({ avatar, user, text, date, isMe }) => {
   return (
-    <div className="message">
-      <div className="message__avatar">
-        <img src={avatar} alt={`Avatar ${user.fullname}`} className="avatar" />
-      </div>
+    <div className={classNames("message", { "message--isme": isMe })}>
       <div className="message__content">
+        <div className="message__avatar">
+          <img
+            src={avatar}
+            alt={`Avatar ${user.fullname}`}
+            className="avatar"
+          />
+        </div>
         <div className="message__info">
           <div className="message__bubble">
             <p className="message__text">{text}</p>
           </div>
-          <span className="message__date">{date}</span>
+          <span className="message__date">
+            {formatDistance(date, new Date(), {
+              addSuffix: true,
+              locale: ruLocale,
+            })}
+          </span>
         </div>
       </div>
     </div>
@@ -28,7 +40,7 @@ Message.defaultProps = {
 Message.propTypes = {
   avatar: PropTypes.string,
   text: PropTypes.string,
-  date: PropTypes.string,
+  date: PropTypes.number,
   user: PropTypes.object,
   attachments: PropTypes.array,
   isMe: PropTypes.bool,
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,8 +6,14 @@ const Home = () => {
     <section className="home">
       <Message
         avatar="https://cdn.vox-cdn.com/thumbor/HEeDWCewhBlypxLbbYbkBfmDc6w=/0x0:1080x718/1200x800/filters:focal(477x288:649x460)/cdn.vox-cdn.com/uploads/chorus_image/image/71263353/300017093_10114630004939621_5854109382330704814_n.0.jpg"
-        text="Салам Брут, че, как, уничтожил флот галлов?"
-        date="Вчера в 21:30"
+        text="Салам Брут, че, как, уничтожил флот галлов? Салам Брут, че, как, уничтожил флот галлов? Салам Брут, че, как, уничтожил флот галлов? Салам Брут, че, как, уничтожил флот галлов? Салам Брут, че, как, уничтожил флот галлов?"
+        date={1234}
+      />
+        <Message
+        avatar="https://www.pcgamesn.com/wp-content/uploads/2020/09/civ5greece.png"
+        text="Салам Цезарь, все норм, да уничтожил флот"
+        date={1111}
+        isMe={true}
       />
     </section>
   );
