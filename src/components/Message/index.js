f6bcf34abccce9053ec9d62a78665ef39f2d6353
adilet berdibekov <berdibekovadilet@gmail.com>
2022-12-28 09:05:15 +0600

    added message component

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-const Message = () => {
+import "./Message.scss";
+
+const Message = ({ avatar, user, text, date }) => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className="message">
+      <div className="message__avatar">
+        <img src={avatar} alt={`Avatar ${user.fullname}`} className="avatar" />
+      </div>
+      <div className="message__content">
+        <div className="message__info">
+          <div className="message__bubble">
+            <p className="message__text">{text}</p>
+          </div>
+          <span className="message__date">{date}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Message
+Message.defaultProps = {
+  user: {},
+};
+
+Message.propTypes = {
+  avatar: PropTypes.string,
+  text: PropTypes.string,
+  date: PropTypes.string,
+  user: PropTypes.object,
+  attachments: PropTypes.array,
+  isMe: PropTypes.bool,
+  isReaded: PropTypes.bool,
+  isTyping: PropTypes.bool,
+  audio: PropTypes.string,
+};
+
+export default Message;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Message from "../../components/Message";
 
 const Home = () => {
   return (
     <section className="home">
-      Home <Link to="signin">Войти</Link>
+      <Message
+        avatar="https://cdn.vox-cdn.com/thumbor/HEeDWCewhBlypxLbbYbkBfmDc6w=/0x0:1080x718/1200x800/filters:focal(477x288:649x460)/cdn.vox-cdn.com/uploads/chorus_image/image/71263353/300017093_10114630004939621_5854109382330704814_n.0.jpg"
+        text="Салам Брут, че, как, уничтожил флот галлов?"
+        date="Вчера в 21:30"
+      />
     </section>
   );
 };
