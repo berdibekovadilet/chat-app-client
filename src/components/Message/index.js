38952ca804d21ddfbbc8d62fe292a6a1fc1f4ca5
adilet berdibekov <berdibekovadilet@gmail.com>
2022-12-28 11:16:17 +0600

    added readed and noreaded displaing

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -4,12 +4,28 @@ import { formatDistance } from "date-fns";
 import ruLocale from "date-fns/locale/ru";
 import classNames from "classnames";
 
+import readedSvg from "../../assets/img/readed.svg";
+import noReadedSvg from "../../assets/img/noreaded.svg";
+
 import "./Message.scss";
 
-const Message = ({ avatar, user, text, date, isMe }) => {
+const Message = ({ avatar, user, text, date, isMe, isReaded }) => {
   return (
     <div className={classNames("message", { "message--isme": isMe })}>
       <div className="message__content">
+        {isMe && isReaded ? (
+          <img
+            className="message__icon-readed"
+            src={readedSvg}
+            alt="Readed icon"
+          />
+        ) : (
+          <img
+            className="message__icon-readed message__icon-readed--no"
+            src={noReadedSvg}
+            alt="No readed icon"
+          />
+        )}
         <div className="message__avatar">
           <img
             src={avatar}
